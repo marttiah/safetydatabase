9be6152663c8fe0a9892d8520276587fe15cdfb8
marttiah <martti.ahtola@gmail.com>
2020-11-15 20:23:58 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE adrs (id SERIAL PRIMARY KEY, dayzero TIMESTAMP, aedescription TEXT, reporter TEXT, patient TEXT);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, company TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 CREATE TABLE countries (country TEXT);
 CREATE TABLE companies (id SERIAL PRIMARY KEY, name TEXT UNIQUE, product TEXT);
