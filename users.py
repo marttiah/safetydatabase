d8c3ac4790f7b45ecbb4c60f10c4af342e615577
marttiah <martti.ahtola@gmail.com>
2020-11-15 20:23:33 +0200

    Create users.py

---
+++ users.py
@@ -0,0 +1,32 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login():
+    sql = "SELECT password, id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone() 
+    if user == None:
+        return False
+    else:
+        if check_password_hash(user[0],password):
+            session["username"] = user[1]
+            return True
+        else:
+             return False
+
+def logout():
+    del session["username"]
+
+def register(username,password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        db.session.execute(sql, {"username":username,"password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return login(username,password)
+
+def username():
+    return session.get("username",0)
