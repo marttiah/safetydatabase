e94dac3b059c13f5c5a4eb0d3cec41a872a2a32f
marttiah <martti.ahtola@gmail.com>
2020-11-18 09:35:46 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,20 +1,22 @@
 from db import db
 import users
 
-def form(aedescription, reporter, product, patient):
-    username = users.username()
-    if username == 0:
-        return False
+def send(aedescription, reporter, product, patient):
+    #username = users.username()
+    #if username == 0:
+        #return False
     sql =  "INSERT INTO adrs (aedescription, reporter, product, patient) VALUES (:aedescription, :reporter, :product, :patient)"
-    db.session.execute(sql, {"aedescription":aedescription, "reporter":reporter, "product":product, "patient":patient})
+    db.session.execute(sql, {"aedescription": aedescription, "reporter": reporter, "product": product, "patient": patient})
     db.session.commit()
     return True
 
 def numberofforms():
     result = db.session.execute("SELECT COUNT (*) FROM adrs")
     count = result.fetchone()[0]
+    return count
 
 def linelisting():
-    result = db.session.execute("SELECT aedescription, reporter, product, patient FROM adrs")
+    result = db.session.execute("SELECT * FROM adrs")
     adrs = result.fetchall()
+    return adrs
     
