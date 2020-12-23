212854145ef42a6ff0e71b4bdf9a5b0bcc13f431
marttiah <martti.ahtola@gmail.com>
2020-12-23 11:21:20 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
-from flask.globals import session
 from app import app
 from flask import render_template, request, redirect
 import users, forms
+import re
 
 @app.route("/")
 def index():
@@ -27,24 +27,39 @@ def login():
         if users.login(username,password):
             return redirect("/main")
         else:
-            return render_template("error.html",error="Wrong username or password")
+            return render_template("error.html",error="Wrong username or password. Please try again.")
 
 @app.route("/logout")
 def logout():
     users.logout()
     return redirect("/")
 
-@app.route("/register", methods=["get","post"])
+@app.route("/register", methods=["GET","POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if users.register(username,password):
-            return redirect("/")
+        repassword = request.form["repassword"]
+
+        if repassword != password:
+            return render_template("register.html",error="Entered passwords do not match. Please try again.") 
+        elif len(password) < 8: 
+            return render_template("register.html",error="Entered password is too short. Please try again.")  
+        elif not re.search("[a-z]", password): 
+            return render_template("register.html",error="Entered password does not include small letters. Please try again.")  
+        elif not re.search("[A-Z]", password): 
+            return render_template("register.html",error="Entered password does not include capital letters. Please try again.")  
+        elif not re.search("[0-9]", password): 
+            return render_template("register.html",error="Entered password does not include numbers. Please try again.")  
+        elif not re.search("[\s]", password): 
+            return render_template("register.html",error="Entered password includes spaces. Please try again.")  
         else:
-            return render_template("error.html",error="Registration was unsuccessful")
+            if users.register(username,password):
+                return redirect("/main")
+            else:
+                return render_template("error.html",error="Registration was unsuccessful. Please try again.")
 
 @app.route("/form")
 def form():
